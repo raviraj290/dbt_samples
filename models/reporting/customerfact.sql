48be0b177fe78026919a87ec7055439a018c2bac
selashya bathula <bathulaselashya@gmail.com>
2025-03-11 15:46:16 +0000

    final-1

--- models/reporting/customerfact.sql
+++ models/reporting/customerfact.sql
@@ -1,5 +1,5 @@
 {{ config(materialized='view') }}
-select count(*)
+select o.*
 from {{ ref('orders') }} o
 inner join {{ ref('customer') }} c on o.O_CUSTKEY = c.C_CUSTKEY
 inner join {{ ref('lineitem') }} l on l.L_ORDERKEY = o.O_ORDERKEY
